4c3f88833d166b90c20f6b2679787be1d61d79cb
Grigory <grigorydavydenko@yandex.ru>
2021-04-26 18:38:27 +0700

    3

--- flask_app.py
+++ flask_app.py
@@ -134,6 +134,5 @@ def get_suggests(user_id):
 
 
 if __name__ == '__main__':
-    if __name__ == '__main__':
-        port = int(os.environ.get("PORT", 5000))
-        app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
