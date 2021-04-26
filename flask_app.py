6ea2187c34a46b19d05302b2e2225cb96b903599
Grigory <grigorydavydenko@yandex.ru>
2021-04-26 18:30:02 +0700

    2

--- flask_app.py
+++ flask_app.py
@@ -1,6 +1,7 @@
 # импортируем библиотеки
 from flask import Flask, request
 import logging
+import os
 
 # библиотека, которая нам понадобится для работы с JSON
 import json
@@ -133,4 +134,6 @@ def get_suggests(user_id):
 
 
 if __name__ == '__main__':
-    app.run()
+    if __name__ == '__main__':
+        port = int(os.environ.get("PORT", 5000))
+        app.run(host='0.0.0.0', port=port)
